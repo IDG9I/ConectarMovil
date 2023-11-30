a6d0da2156f50782c43ef45b28471f7a31c6d97c
IDG9I <damiangamerxd123@gmail.com>
2023-11-30 17:41:17 -0300

    Initial commit

---
+++ app/src/main/java/com/example/conectamvil/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.conectamvil;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btnIniciar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnIniciar = (Button)findViewById(R.id.btnIniciar);
+
+        btnIniciar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent inten = new Intent(MainActivity.this, Menu.class);
+                startActivity(inten);
+            }
+        });
+    }
+}
