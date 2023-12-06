1007680948417fcff4841fca546fe529751019f8
IDG9I <damiangamerxd123@gmail.com>
2023-12-06 20:11:48 -0300

    2.5V Icono

--- app/src/main/java/com/example/conectamvil/Menu.java
+++ app/src/main/java/com/example/conectamvil/Menu.java
@@ -4,17 +4,26 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Menu extends AppCompatActivity {
+    Button btnAgregarUser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+
+        btnAgregarUser =(Button)findViewById(R.id.btnAgregarUser);
+
+        btnAgregarUser.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Menu.this, Agregar.class);
+                startActivity(intent);
+            }
+        });
     }
 
-    public void btnAgregars(){
-        Intent intent = new Intent(Menu.this, Agregar.class);
-        startActivity(intent);
-    }
 }
